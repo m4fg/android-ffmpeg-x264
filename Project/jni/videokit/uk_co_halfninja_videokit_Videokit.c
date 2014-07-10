2cef91f6593a94f24b94a9245bbd9262b22c14d2
yuuma <tsuji@essentia.co.jp>
2014-07-10 15:15:28 +0900

    戻す

--- Project/jni/videokit/uk_co_halfninja_videokit_Videokit.c
+++ Project/jni/videokit/uk_co_halfninja_videokit_Videokit.c
@@ -22,8 +22,6 @@ JNIEXPORT void JNICALL Java_uk_co_halfninja_videokit_Videokit_run(JNIEnv *env, j
 	char **argv = NULL;
 	jstring *strr = NULL;
 
-	char *tmp = NULL;
-
 	if (args != NULL) {
 		argc = (*env)->GetArrayLength(env, args);
 		argv = (char **) malloc(sizeof(char *) * argc);
@@ -32,10 +30,7 @@ JNIEXPORT void JNICALL Java_uk_co_halfninja_videokit_Videokit_run(JNIEnv *env, j
 		for(i=0;i<argc;i++)
 		{
 			strr[i] = (jstring)(*env)->GetObjectArrayElement(env, args, i);
-			tmp = (char *)(*env)->GetStringUTFChars(env, strr[i], 0);
-			argv[i] = (char *)malloc(sizeof(char) * strlen(tmp));
-			strcpy(argv[i], tmp);
-			(*env)->ReleaseStringUTFChars(env, strr[i], tmp);
+			argv[i] = (char *)(*env)->GetStringUTFChars(env, strr[i], 0);
 		}
 	}	
 
@@ -43,8 +38,7 @@ JNIEXPORT void JNICALL Java_uk_co_halfninja_videokit_Videokit_run(JNIEnv *env, j
 	
 	for(i=0;i<argc;i++)
 	{
-		//(*env)->ReleaseStringUTFChars(env, strr[i], argv[i]);
-		free(argv[i]);
+		(*env)->ReleaseStringUTFChars(env, strr[i], argv[i]);
 	}
 	free(argv);
 	free(strr);
